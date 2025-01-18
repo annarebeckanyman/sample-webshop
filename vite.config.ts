84bea611b036c26246d181b672242ab5eef76769
Anna Rehnman <anna.rehnman@afry.com>
2025-01-18 12:58:04 +0100

    update vite config and add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@api': path.resolve('src/api'),
+      '@app': path.resolve('src/app'),
+      '@components': path.resolve('src/components'),
+      '@features': path.resolve('src/features'),
+      '@pages': path.resolve('src/pages'),
+      '@typings': path.resolve('src/typings'),
+      '@utils': path.resolve('src/utils'),
+    },
+  },
 })
