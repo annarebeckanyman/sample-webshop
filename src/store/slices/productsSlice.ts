3139577728afcbc655fabd8d9426bda3c3606339
Anna Rehnman <anna.rehnman@afry.com>
2025-01-19 20:10:09 +0100

    add selected product to product slice

--- src/store/slices/productsSlice.ts
+++ src/store/slices/productsSlice.ts
@@ -1,30 +1,30 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
-import { Work } from '@typings/products.types'
+import { CartItem, ProductDetails } from '@typings/product.types'
 
 type ProductsState = {
-  shoppingCart: Work[]
-  currentProduct: Work | null
+  shoppingCart: CartItem[]
+  selectedProduct: ProductDetails | null
 }
 
 const initialState: ProductsState = {
   shoppingCart: [],
-  currentProduct: null,
+  selectedProduct: null,
 }
 
 export const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    setShoppingCart: (state, action: PayloadAction<Work[]>) => {
+    setShoppingCart: (state, action: PayloadAction<CartItem[]>) => {
       state.shoppingCart = action.payload
     },
-    setCurrentProduct: (state, action: PayloadAction<Work>) => {
-      state.currentProduct = action.payload
+    setSelectedProduct: (state, action: PayloadAction<ProductDetails | null>) => {
+      state.selectedProduct = action.payload
     },
   },
 })
 
-export const { setShoppingCart, setCurrentProduct } = productsSlice.actions
+export const { setShoppingCart, setSelectedProduct } = productsSlice.actions
 
 export default productsSlice.reducer
