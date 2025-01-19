7baeeced3402e170f9491534c5c973121456238f
Anna Rehnman <anna.rehnman@afry.com>
2025-01-19 15:23:03 +0100

    update layout styling

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,6 @@
-import { AppShell, BackgroundImage, Container, Paper, useMantineTheme } from '@mantine/core'
+import { AppShell, Container, Paper, useMantineTheme } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
 import Header from '@components/header/Header'
-import bgImage from '../assets/gard.jpg'
 import classes from './styles/layout.module.css'
 import { useMediaQuery } from '@mantine/hooks'
 
@@ -10,19 +9,17 @@ export default function Layout() {
   const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.xs})`)
 
   return (
-    <AppShell header={{ height: isMobile ? 72 : 98 }} withBorder={false}>
-      <BackgroundImage src={bgImage} className={classes.bgImage}>
-        <AppShell.Header className={classes.header}>
-          <Header />
-        </AppShell.Header>
-        <AppShell.Main>
-          <Container h="inherit" size="xl" mt="xl" mb="xl">
-            <Paper radius="md" p="xl">
-              <Outlet />
-            </Paper>
-          </Container>
-        </AppShell.Main>
-      </BackgroundImage>
+    <AppShell header={{ height: isMobile ? 72 : 98 }} withBorder={false} style={{ backgroundColor: 'gray' }}>
+      <AppShell.Header className={classes.header}>
+        <Header />
+      </AppShell.Header>
+      <AppShell.Main>
+        <Container h="inherit" size="xl">
+          <Paper radius="md" p="xl" my="xl">
+            <Outlet />
+          </Paper>
+        </Container>
+      </AppShell.Main>
     </AppShell>
   )
 }
