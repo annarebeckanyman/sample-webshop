b1c25ba620a1ab8ef9b95bc78a003afdc7663c33
Anna Rehnman <anna.rehnman@afry.com>
2025-01-20 12:49:47 +0100

    update gitignore and remove unused variables

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,3 @@
 namespace NodeJS {
-  interface ProcessEnv {
-    MAPBOX_ACCESS_TOKEN: string
-    FIREBASE_API_KEY: string
-    FIREBASE_AUTH_DOMAIN: string
-    FIREBASE_PROJECT_ID: string
-    FIREBASE_STORAGE_BUCKET: string
-    FIREBASE_MESSAGING_SENDER_ID: string
-    FIREBASE_APP_ID: string
-  }
+  interface ProcessEnv {}
 }
