b9d26c58fe52e03d47d2417f3e73217036d4c77f
Anna Rehnman <anna.rehnman@afry.com>
2025-01-20 17:44:47 +0100

    add empty cart button to checkout page

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,9 +1,15 @@
-import { Stack, Text, Title } from '@mantine/core'
+import { Button, Group, Stack, Text, Title } from '@mantine/core'
 import { nanoid } from '@reduxjs/toolkit'
-import { useAppSelector } from '@store/hooks'
+import { useAppDispatch, useAppSelector } from '@store/hooks'
+import { setShoppingCart } from '@store/slices/productsSlice'
 
 export default function CheckoutPage() {
   const { shoppingCart } = useAppSelector((state) => state.products)
+  const dispatch = useAppDispatch()
+
+  const handleEmptyCart = () => {
+    dispatch(setShoppingCart([]))
+  }
 
   return (
     <Stack>
@@ -18,6 +24,13 @@ export default function CheckoutPage() {
         </>
       ) : (
         <Text>No products added</Text>
+      )}
+      {shoppingCart.length > 0 && (
+        <Group>
+          <Button onClick={handleEmptyCart} color="#E07A5F" radius="xs">
+            Empty Cart
+          </Button>
+        </Group>
       )}
     </Stack>
   )
