9424edf8a43b1e1b31b76ad0c8a1e90cd6977dc7
Anna Rehnman <anna.rehnman@afry.com>
2025-01-21 17:40:57 +0100

    moved api url to env

--- src/api/booksSlice.ts
+++ src/api/booksSlice.ts
@@ -3,7 +3,7 @@ import { ProductResponse, ProductsResponse } from '@typings/product.types'
 
 export const booksApi = createApi({
   reducerPath: 'bookApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://openlibrary.org' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_WORKS_API_URL }),
   endpoints: (builder) => ({
     getBooksBySubject: builder.query<ProductsResponse, string>({
       query: (subject) => `/subjects/${subject}.json`,
