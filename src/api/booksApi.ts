de148c1f4b405d080f969f5c8b589b92398c50d5
Anna Rehnman <anna.rehnman@afry.com>
2025-02-21 15:59:45 +0100

    remove api key from reducers

--- src/api/booksApi.ts
+++ src/api/booksApi.ts
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { BookItem, BooksResponse } from '@typings/book.types'
 
-const apiKey = import.meta.env.VITE_API_KEY
-
 const filterSellableItems = (items?: BookItem[]) => items?.filter((item) => item.saleInfo.saleability !== 'FREE') || []
 
 const getSellableResponse = (response: BooksResponse) => {
@@ -22,16 +20,16 @@ export const booksApi = createApi({
   }),
   endpoints: (builder) => ({
     getAllBooks: builder.query<BooksResponse, void>({
-      query: () => `/volumes?q=*&maxResults=40&key=${apiKey}`,
+      query: () => `/volumes?q=*&maxResults=40`,
       transformResponse: (response: BooksResponse) => getSellableResponse(response),
     }),
     getBooksBySubject: builder.query<BooksResponse, string>({
-      query: (category) => `/volumes?q=*+subject:${category}&maxResults=40&key=${apiKey}`,
+      query: (category) => `/volumes?q=*+subject:${category}&maxResults=40`,
       transformResponse: (response: BooksResponse) => getSellableResponse(response),
     }),
     getBookById: builder.query<BookItem, string>({
       query: (id) => {
-        return `/volumes/${id}&key=${apiKey}`
+        return `/volumes/${id}`
       },
       transformResponse: (response: BookItem) => {
         if (response.saleInfo.saleability !== 'FREE') {
